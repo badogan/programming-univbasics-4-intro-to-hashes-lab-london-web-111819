e486af8511a8d90198a977b3b7d2925ddc2e24f1
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 13:49:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,12 +3,11 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
   m_hash = {favourite_day:"Friday"}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  m_hash = {:name:'Grace Hopper'}
 end
 
 def id_generator
