ed195dce1618b066643dc33e9e4c7ce7bb322a50
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 13:55:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,5 +28,6 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key]
     hash[key] = hash[key] + 
-    
+  else
+    hash[key] = 1 
 end
